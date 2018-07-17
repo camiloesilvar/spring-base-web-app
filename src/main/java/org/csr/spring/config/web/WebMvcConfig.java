51bafa8a69ebbe844467cdbe9da22dedce9c08f5
Camilo <camiloe.silvar@gmail.com>
2018-07-16 22:05:02 -0400

    Configuración Spring MVC basica
    Aplicación funcional. Etapa de configuracion.

---
+++ src/main/java/org/csr/spring/config/web/WebMvcConfig.java
@@ -0,0 +1,11 @@
+package org.csr.spring.config.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+
+}
