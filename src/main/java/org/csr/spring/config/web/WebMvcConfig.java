94821ee6bf8323b7c8cb9f2e47f8db05ce11bdce
Camilo <camiloe.silvar@gmail.com>
2018-07-17 22:53:57 -0400

    Configuración de la pagina de inicio HTML

--- src/main/java/org/csr/spring/config/web/WebMvcConfig.java
+++ src/main/java/org/csr/spring/config/web/WebMvcConfig.java
@@ -2,10 +2,21 @@ package org.csr.spring.config.web;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**").addResourceLocations("/static/");
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("forward:/inicio.html");
+	}
 }
