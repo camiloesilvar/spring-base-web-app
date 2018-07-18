754a26a566c6df9e0203a8361b7c73eb593d4fe9
Camilo <camiloe.silvar@gmail.com>
2018-07-17 23:07:00 -0400

    Adición de una clase "@Controller"
    Clase basica y simple de un controlador Spring

--- src/main/java/org/csr/spring/config/web/WebMvcConfig.java
+++ src/main/java/org/csr/spring/config/web/WebMvcConfig.java
@@ -1,13 +1,16 @@
 package org.csr.spring.config.web;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
+@ComponentScan(basePackages = { "org.csr.spring.front.controller" })
 public class WebMvcConfig implements WebMvcConfigurer {
 
 	@Override
@@ -18,5 +21,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("forward:/inicio.html");
+	}
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.jsp().prefix("/WEB-INF/views/").suffix(".jsp");
 	}
 }
